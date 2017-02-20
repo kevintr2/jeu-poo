41d20dcb2e8b6bc6161b9ac00145009f62f053b9
Kevin Tran <kevintran@hotmail.de>
2017-02-20 18:48:35 +0100

    index page + donjon page

---
+++ donjon.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jeu</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="landing">
+    <div class="character-list">
+        <div class="radio">
+            <label for="guerrier"><input type="radio" name="guerrier"> Guerrier</label>
+        </div>
+        <div class="radio">
+            <label for="magicien"><input type="radio" name="magicien"> Magicien</label>
+        </div>
+        <div class="radio">
+            <label for="paladin"><input type="radio" name="paladin"> Paladin</label>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
